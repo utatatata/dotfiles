208280029079667bcc8d7498b15ad8409c6248f5
utakuma <40954814+utatatata@users.noreply.github.com>
2023-07-26 21:31:36 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,17 @@ abbr '......' 'cd ../../../../..'
 abbr '.......' 'cd ../../../../../..'
 abbr '........' 'cd ../../../../../../..'
 abbr '.........' 'cd ../../../../../../../..'
+
+
+
+#################### WRITE SETTINGS ABOVE ####################
+
+#
+# Start X
+#
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
